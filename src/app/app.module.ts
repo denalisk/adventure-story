f6b06b8fff768740743c417b38d06692ffc86f99
Alexandra Holcombe and Sam Kirsch <alexandra+sam@>
2017-04-04 09:22:07 -0700

    Add scenario, player models, failure, player-create, scenario, start, success components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { ScenarioComponent } from './scenario/scenario.component';
+import { SuccessComponent } from './success/success.component';
+import { FailureComponent } from './failure/failure.component';
+import { PlayerCreateComponent } from './player-create/player-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    ScenarioComponent,
+    SuccessComponent,
+    FailureComponent,
+    PlayerCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scenario.model.ts
@@ -0,0 +1,3 @@
+export class Scenario {
+  constructor(public description: string, public id: number, public optionDescription: string, public optionArray: Scenario[]){}
+}
